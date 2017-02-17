80883442a2ef50156f3ca845d434d33690531a90
goromal <andrew.torgesen@gmail.com>
2017-02-17 00:31:58 -0700

    basic waypoint display added

--- src/ground_station/marble_map.py
+++ src/ground_station/marble_map.py
@@ -1,13 +1,52 @@
 from PyKDE4.marble import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
 import map_info_parser
 #import rospy
 #from fcu_common.msg import FW_Waypoint
 #import numpy as np
 
+'''
+For changing color of current waypoint to green:
+Use the QTimer (as shown in the layer tutorial) to call the marble.update method
+at every set interval [[in fact, do it like map_widget's Plane() class, but in this
+MarbleMap() class]],
+and have PaintLayer have an internal state-subscriber-like class whose data member
+of current waypoint is always updating.
+'''
+# Class for allowing the widget to paint to the marble map
+class PaintLayer(Marble.LayerInterface, QObject):
+    def __init__(self, marble):
+        QObject.__init__(self)
+        self.marble = marble
+        ''' This is only a temporary object; should be replaced by an xml file or something
+            These are the competition waypoints, and are currently set to always be drawn
+            at the specified latlong coordinates
+        '''
+        self.waypoints = [(38.14531389,-76.42911944),
+        (38.149222,-76.4294833),(38.150133,-76.4308556),(38.14895,-76.4322861),
+        (38.147011,-76.4306417),(38.1437833,-76.4319944)]
+
+    def renderPosition(self): # ??
+        return ['SURFACE']
+
+    def render(self, painter, viewPort, renderPos, layer):
+        painter.m_index = 0 # should provide a surface paint
+        painter.setRenderHint(QPainter.Antialiasing, True)
+        painter.setPen(QPen(QBrush(Qt.red), 4.5, Qt.SolidLine, Qt.RoundCap))
+        # Draw waypoints according to latlong degrees
+        for waypoint in self.waypoints:
+            location = Marble.GeoDataCoordinates(waypoint[1], waypoint[0], 0.0, Marble.GeoDataCoordinates.Degree)
+            painter.drawEllipse(location, 5, 5)
+
+        return True
+
 class MarbleMap(Marble.MarbleWidget):
     def __init__(self, parent=None): # Parent line VERY important
         super(MarbleMap, self).__init__() # MarbleWidget constructor
-        self.setMapThemeId("earth/openstreetmap/openstreetmap.dgml")
+        #self.setMapThemeId("earth/satelliteview/bluemarble.dgml") # satellite, doesn't load very well
+        self.setMapThemeId("earth/openstreetmap/openstreetmap.dgml") # street view
         self.setProjection(Marble.Mercator)
         self.setShowOverviewMap(False)
 
@@ -16,6 +55,9 @@ class MarbleMap(Marble.MarbleWidget):
         self._home_pt = Marble.GeoDataCoordinates(def_latlonzoom[1], def_latlonzoom[0], 0.0, Marble.GeoDataCoordinates.Degree) # +
         self.centerOn(self._home_pt)
         self.setZoom(def_latlonzoom[2])
+
+        paintlayer = PaintLayer(self)
+        self.addLayer(paintlayer)
 
         #rospy.Subscriber("/waypoint_path", FW_Waypoint, self.callback_waypoints)
         # waypoint buffers
